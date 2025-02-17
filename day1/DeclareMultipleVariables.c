8de7cde137b51c91371bdf44653b453c13f2f678
Lijon <134403855+lejon369@users.noreply.github.com>
2025-02-17 14:39:15 +0600

    multipule variable

---
+++ day1/DeclareMultipleVariables.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+ int main (){
+    int a = 3,b = 6, c = 9;
+    printf("\n%d\n\n", a + b + c);
+
+    int x,y,z;
+    x = y = z = 369;
+    printf("%d\n",x+y+z);
+
+
+    return 0;
+ }
