3f9457889805bf3d7d48799589667a57ac33ddfd
Lijon <134403855+lejon369@users.noreply.github.com>
2025-02-17 14:20:17 +0600

    the quick brown fox ump over the lazy dogs

---
+++ day1/cFormatSpecifier.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+
+    int intNunber= 369;
+    float floatNumber = 59.558;
+    char charter = 'L';
+
+    printf("this float number is : %d\n\n",intNunber );
+    printf("this float number is : %f\n\n",floatNumber );
+    printf("this float number is : %c\n\n",charter );
+    return 0 ;
+};
