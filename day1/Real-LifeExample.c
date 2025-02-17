aeb22ea262c24d516fd0765a693f93029812d925
Lijon <134403855+lejon369@users.noreply.github.com>
2025-02-17 14:47:10 +0600

    rea life example

---
+++ day1/Real-LifeExample.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+ int main (){
+   int stdentId = 251034004;
+   float fish = 47.0420;
+   char name = 'L';
+
+     printf("this is a student id is : %d\n",stdentId);
+     printf("this is a fee: %f\n",fish);
+     printf("this is a name: %c\n", name);
+
+    return 0;
+ }
